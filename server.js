3f4f42389c4b0e314b90471fe8b329ae9a29ea9b
dphengsiaroun <dphengsiaroun@outlook.fr>
2018-05-25 17:07:13 +0200

    refactor

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-var app = require('express')()
+const express = require('express')
+const app = express()
 
-app.get('/',function(req,res) {
-  res.send("Hello World")
-})
+app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(3000)
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
